57073b84bf98b094da86dc9994a729415876c547
guominglong <guominglong@51talk.com>
2017-04-13 18:17:55 +0800

    简单实现BaseProxy

--- www/js/engine/BaseProxy.js
+++ www/js/engine/BaseProxy.js
@@ -2,3 +2,14 @@
  * 所有委托的基类
  * Created by guominglong on 2017/4/7.
  */
+class BaseProxy extends BaseEventDispatcher{
+    constructor(){
+        super();
+        this.delegate = null;
+    }
+
+    destroy(){
+        super.destroy();
+        this.delegate = null;
+    }
+}
